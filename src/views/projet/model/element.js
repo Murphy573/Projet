b62e0fc3355ad93c7cb9479d14cb619f1189d98b
Murphy573 <429655198@qq.com>
2020-08-05 17:57:46 +0800

    doc：加样式注释

--- src/views/projet/model/element.js
+++ src/views/projet/model/element.js
@@ -8,6 +8,7 @@ import { copyCommonCss } from './css';
 export function InitNewElement (elementData) {
   let _elementData = deepClone(elementData);
   _elementData.uid = generateUniqueIdByCustom(12);
+  // 用组件定义的defaultCss值覆盖默认样式
   _elementData.css = Object.assign({}, copyCommonCss(), _elementData.defaultCss);
   delete _elementData.defaultCss;
   return _elementData;
