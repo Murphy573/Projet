c41ccf7dd3595ca7c2dff838d5222afa820e5f57
Murphy573 <429655198@qq.com>
2020-08-12 14:21:10 +0800

    style：优化projet用到的全局样式加载时机

--- src/views/projet/layout/index.vue
+++ src/views/projet/layout/index.vue
@@ -34,3 +34,18 @@ export default {
   }
 };
 </script>
+
+<style lang="scss">
+.full-width {
+  width: 100%;
+}
+.el-form {
+  .el-form-item {
+    // margin-bottom: 30px !important;
+
+    &.el-form-item-bottom-0 {
+      margin-bottom: 0 !important;
+    }
+  }
+}
+</style>
